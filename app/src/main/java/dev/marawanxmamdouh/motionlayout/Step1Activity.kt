23eaff22d87029299763f44bd953dfa5bb0e720d
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-22 00:58:24 +0200

    Create the project

---
+++ app/src/main/java/dev/marawanxmamdouh/motionlayout/Step1Activity.kt
@@ -0,0 +1,12 @@
+package dev.marawanxmamdouh.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Step1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step1)
+    }
+}
